8c4b926b4b8b60cf3aad5a680b2664397943abb6
segin-GH <segin.bytes@gmail.com>
2022-10-31 16:25:40 +0530

    ..

---
+++ c++_for_advance/virtualFunction/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+class Entity
+{
+    public:
+
+        std::string GetName() {return "Entity";};
+};
+
+class Player : public Entity
+{
+    private:
+        std::string m_Name;
+    
+    public:
+        Player(const std::string &name)
+            : m_Name(name) {};
+        std::string GetName() {return m_Name;}
+
+};
+
+
+int main()
+{
+    Entity *e = new Entity();
+    std::cout << e ->GetName() << std::endl;
+
+    Player *p = new Player("segin");
+    std::cout << p ->GetName();
+}
