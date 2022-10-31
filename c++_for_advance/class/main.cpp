3b0c94f12d3baacc76ae341ce83b8f9faf16ca9c
segin-GH <segin.bytes@gmail.com>
2022-10-31 09:35:05 +0530

    .

--- c++_for_advance/class/main.cpp
+++ c++_for_advance/class/main.cpp
@@ -1,7 +1,32 @@
 # include <iostream>
 
+namespace ply {
+class Player //  "Player" <- this is obj 
+{
+    public:
+        int x, y;   
+        int speed;
+
+    void Move(int xa, int ya) 
+    {
+        //do some stuff
+        x += xa;
+        y += ya;
+        std::cout << "player moved" << std::endl;
+
+    }
+};
+};
+
 
 int main()
 {
-    //
+    ply::Player player; //   <- this is called instance
+
+    player.x = 5;
+    player.y = 7;
+    player.speed = 1000;
+
+    player.Move(10,10);
+
 }
