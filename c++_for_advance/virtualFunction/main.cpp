c5652bb96edb5c33b87676b4234fda80901e6752
segin-GH <segin.bytes@gmail.com>
2022-10-31 19:43:22 +0530

    .

--- c++_for_advance/virtualFunction/main.cpp
+++ c++_for_advance/virtualFunction/main.cpp
@@ -1,31 +1,47 @@
 #include <iostream>
 #include <string>
+#include <ostream>
 
 class Entity
 {
     public:
 
-        std::string GetName() {return "Entity";};
+       virtual std::string GetName() {return "Entity";};
 };
 
 class Player : public Entity
 {
     private:
         std::string m_Name;
-    
+
     public:
         Player(const std::string &name)
             : m_Name(name) {};
-        std::string GetName() {return m_Name;}
-
+        std::string GetName() override {return m_Name;}
 };
+
+void PrintName(Entity *entity)
+{
+    std::cout << entity->GetName() << std::endl;
+}
 
 
 int main()
 {
     Entity *e = new Entity();
-    std::cout << e ->GetName() << std::endl;
+    // std::cout << e ->GetName() << std::endl;
+    PrintName(e);
 
-    Player *p = new Player("segin");
-    std::cout << p ->GetName();
+    Player *p = new Player("luffy");
+    // std::cout << p ->GetName() << std::endl;
+    PrintName(p);
+
+// if you start refering player as if it was an entity (polymorphism)
+
+    Entity * entity = p;
+    // std::cout << entity->GetName() << std::endl;
+    PrintName(p);
+
+
+
 }
