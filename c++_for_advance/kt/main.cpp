81a08de8e874a176ded6dab410ac00429b1e7b02
segin-GH <segin.bytes@gmail.com>
2022-11-27 22:40:27 +0530

    .

---
+++ c++_for_advance/kt/main.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+# include<string>
+
+bool answer = true;
+
+int main(void)
+{
+	std::string savage_level = answer ? "high":"low";
+	std::cout << "savage level "<<  savage_level << std::endl;
+}
--- c++_for_advance/kt/virtualfunc/main.cpp
+++ c++_for_advance/kt/virtualfunc/main.cpp
@@ -1,13 +1,11 @@
-# include<iostream>
-#include<string>
-
+#include <iostream>
+#include <string>
 class Entity
 {
 public:
-    virtual std::string get_name() {return "Entity";}
+     virtual std::string get_name(){return "Entity";};
+
 };
-
-
 
 class Player : public Entity
 {
@@ -15,21 +13,30 @@ private:
     std::string m_name;
 
 public:
-    Player(const std::string &name)
+    Player(const std::string &name )
         : m_name(name){};
+
     std::string get_name() override {return m_name;}
+
 };
 
 
+void PrintName(Entity *ent)
+{
+    std::cout << ent -> get_name() <<std::endl;
+}
 
-int main()
+int main(void)
 {
     Entity *e = new Entity();
-    std::cout << e -> get_name() << std::endl;
+    // std::cout << e -> get_name() << std::endl;
+    PrintName(e);
 
     Player *p = new Player("dude");
-    std::cout << p -> get_name() << std::endl;
+    // std::cout << p -> get_name() << std::endl;
+    PrintName(p);
 
-    Entity * ent = p;
-    std::cout << ent -> get_name() <<std::endl;
+    Entity *entity = p;
+    // std::cout << entity->get_name() << std::endl;
+    PrintName(p);
 }
